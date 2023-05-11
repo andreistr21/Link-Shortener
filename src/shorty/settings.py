41cd2eb82a410a4e8b121e11adac038659a8bbac
andreistr21 <andstra45@gmail.com>
2023-05-11 03:55:53 +0200

    base template

--- src/shorty/settings.py
+++ src/shorty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8q+^2&g9mbg5+dyvkpj_#7r+h0=csq4t8k@1@014iy7898a#j-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = "shorty.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
