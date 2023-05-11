1dacd7a65a6f49122d99004c798242231f5e78a3
andreistr21 <andstra45@gmail.com>
2023-05-11 16:36:16 +0200

    account app, profile model

---
+++ src/account/models.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+    last_online = models.DateField()
+
+    @property
+    def update_last_online(self) -> None:
+        self.last_online = timezone.now()
