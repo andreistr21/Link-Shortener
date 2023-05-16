2d7f6c8ed26f523f96f042a5c2098138550d40e3
andreistr21 <andstra45@gmail.com>
2023-05-16 03:01:26 +0200

    added tests for link_validation service

---
+++ src/shortener/tests/services/test_link_validation.py
@@ -0,0 +1,62 @@
+from unittest import TestCase
+from django.conf import settings
+
+from shortener.forms import ShortenForm
+from shortener.services import link_validation
+
+
+class LinkValidationTests(TestCase):
+    def test_if_link_valid(self):
+        valid_links = [
+            "https://www.youtube.com/",
+            "https://youtube.com/",
+            "www.youtube.com/",
+        ]
+
+        for link in valid_links:
+            shorten_form = ShortenForm(data={"long_link": link})
+            shorten_form.is_valid()
+
+            self.assertTrue(link_validation(shorten_form))
+
+    def test_if_link_invalid(self):
+        shorten_form = ShortenForm(data={"long_link": "youtube"})
+        shorten_form.is_valid()
+
+        self.assertFalse(link_validation(shorten_form))
+
+    def test_if_link_invalid_expect_form_error(self):
+        error_value = "Enter a valid link"
+        shorten_form = ShortenForm(data={"long_link": "youtube"})
+        shorten_form.is_valid()
+
+        link_validation(shorten_form)
+        errors = shorten_form.errors.get("long_link")
+        self.assertTrue(error_value in errors)
+        
+    def test_with_restricted_domain(self):
+        restricted_links = [
+            "127.0.0.1:8000",
+            "https://127.0.0.1:8000/account",
+        ]
+
+        for link in restricted_links:
+            shorten_form = ShortenForm(data={"long_link": link})
+            shorten_form.is_valid()
+
+            self.assertFalse(link_validation(shorten_form))
+
+    def test_with_restricted_domain_expect_form_error(self):
+        restricted_links = [
+            "127.0.0.1:8000",
+            "https://127.0.0.1:8000/account",
+        ]
+        error_value = "This domain is banned"
+
+        for link in restricted_links:
+            shorten_form = ShortenForm(data={"long_link": link})
+            shorten_form.is_valid()
+            link_validation(shorten_form)
+
+            errors = shorten_form.errors.get("long_link")
+            self.assertTrue(error_value in errors)
