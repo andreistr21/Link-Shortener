92780414e595b43f4b42e80550022646079b1377
andreistr21 <andstra45@gmail.com>
2023-05-17 06:03:52 +0200

    added tests for ShortenForm form

---
+++ src/shortener/tests/forms/test_shorten_form.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from shortener.forms import ShortenForm
+
+
+class ShortenFormTests(TestCase):
+    def test_long_link_field_attributes(self):
+        shorten_form = ShortenForm()
+
+        self.assertInHTML(
+            '<input type="text" name="long_link" placeholder="Enter a long link to make a short one" autofocus="" maxlength="2000" required="" id="id_long_link">',
+            str(shorten_form),
+        )
+
+    def test_alias_field_attributes(self):
+        shorten_form = ShortenForm()
+
+        self.assertInHTML(
+            '<input type="text" name="alias" placeholder="Alias (Optional)" id="id_alias">',
+            str(shorten_form),
+        )
