5b887290ccf8b2eab1a5ac3ddee96dad92606c56
andreistr21 <andstra45@gmail.com>
2023-05-22 16:18:45 +0200

    added confirmation email on request

--- src/account/urls.py
+++ src/account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("sign-up/", views.sign_up, name="sing_up"),
     path("sign-in/", views.sign_in, name="sing_in"),
     path("confirm-email/", views.confirm_email, name="confirm_email"),
+    path("new-confirmation-link/", views.new_confirmation_link, name="new_confirmation_link"),
     path("activate-email/<pk>/<token>", views.activate_email, name="activate_email"),
     path("overview", views.overview, name="overview"),
 ]
--- src/account/views.py
+++ src/account/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import login
-from django.shortcuts import redirect, render
 from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from django.contrib.auth.forms import AuthenticationForm
 
 from account.forms import SignInForm, SignUpForm
 from account.tasks import send_activation_email
@@ -35,14 +35,25 @@ def sign_up(request):
 
 
 def sign_in(request):
+    new_activation_link = False
     sign_in_form = SignInForm()
     if request.POST:
         sign_in_form = SignInForm(request, request.POST)
         if sign_in_form.is_valid():
             login(request, sign_in_form.user_cache)
             return redirect(reverse("account:overview"))
+        elif "Email for this account not confirmed." in sign_in_form.non_field_errors()[0]:
+            new_activation_link = True
+            print(f"{new_activation_link=}")
 
-    return render(request, "account/sign_in.html", {"sign_in_form": sign_in_form})
+    return render(
+        request,
+        "account/sign_in.html",
+        {
+            "sign_in_form": sign_in_form,
+            "new_activation_link": new_activation_link,
+        },
+    )
 
 
 def confirm_email(request):
@@ -67,5 +78,32 @@ def activate_email(_, pk, token):
     return redirect(reverse("shortener:index"))
 
 
-def overview(request):
+def overview(request: HttpRequest) -> HttpResponse:
     return render(request, "account/overview.html", {})
+
+
+def new_confirmation_link(request: HttpRequest) -> HttpResponse:
+    new_confirmation_link_form = SignInForm()
+    if request.POST:
+        new_confirmation_link_form = SignInForm(request, request.POST)
+        if (
+            not new_confirmation_link_form.is_valid()
+            and new_confirmation_link_form.non_field_errors
+            and "Email for this account not confirmed." in new_confirmation_link_form.non_field_errors()[0]
+        ):
+            user = get_object_or_404(Profile, email=new_confirmation_link_form.cleaned_data.get("email"))
+            send_activation_email.delay(
+                domain=get_current_site(request).domain,
+                protocol=request.is_secure(),
+                user_id=user.id,
+                to_email=new_confirmation_link_form.cleaned_data.get("email"),
+            )
+            return redirect(reverse("account:confirm_email"))
+
+    return render(
+        request,
+        "account/new_confirmation_link.html",
+        {
+            "new_confirmation_link_form": new_confirmation_link_form,
+        },
+    )
