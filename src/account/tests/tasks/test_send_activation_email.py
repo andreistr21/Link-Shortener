9d2ad9ce0bb78003676b3f1336fdddc9c6bb9484
andreistr21 <andstra45@gmail.com>
2023-05-26 15:42:24 +0200

    added tests for send activation email task

---
+++ src/account/tests/tasks/test_send_activation_email.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from account.models import Profile
+
+from account.tasks import construct_email, send_activation_email
+from unittest import mock
+
+
+class SendActivationEmailTests(TestCase):
+    def setUp(self):
+        Profile.objects.all().delete()
+        self.domain = "example.com"
+        self.protocol = True  # Assuming the protocol is HTTPS
+        self.to_email = "test@example.com"
+        self.user = Profile.objects.create_user(
+            email="test@gmail.com",
+            password="test_password",
+        )
+
+    @mock.patch("django.core.mail.message.EmailMessage.send")
+    def test_email_sent(self, send_mock):
+        send_activation_email.apply(
+            args=(
+                self.domain,
+                self.protocol,
+                self.user.pk,
+                self.to_email,
+            ),
+            throw=True,
+        )
+
+        send_mock.assert_called_once()
