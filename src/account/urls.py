b8fb0f0df479eb9d74623377d373179786dffc3f
andreistr21 <andstra45@gmail.com>
2023-06-02 18:28:38 +0200

    added logout feature

--- src/account/urls.py
+++ src/account/urls.py
@@ -3,11 +3,12 @@ from django.contrib.auth.views import (
     PasswordResetConfirmView,
     PasswordResetDoneView,
     PasswordResetView,
+    LogoutView,
 )
 from django.urls import include, reverse_lazy
 from django.urls import path
 
-from account.forms import ResetPasswordForm
+from account.forms import ResetPasswordEmailForm, ResetPasswordForm
 
 from . import views
 
@@ -15,6 +16,11 @@ app_name = "account"
 urlpatterns = [
     path("sign-up/", views.sign_up, name="sign_up"),
     path("sign-in/", views.sign_in, name="sign_in"),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="account/logged_out.html"),
+        name="logout",
+    ),
     path("confirm-email/", views.confirm_email, name="confirm_email"),
     path(
         "new-confirmation-link/",
@@ -30,7 +36,7 @@ urlpatterns = [
     path(
         "password-reset/",
         PasswordResetView.as_view(
-            form_class=ResetPasswordForm,
+            form_class=ResetPasswordEmailForm,
             template_name="account/password_reset.html",
             email_template_name="account/email_password_reset_email.html",
             success_url=reverse_lazy("account:password_reset_done"),
--- src/static/main.js
+++ src/static/main.js
@@ -12,3 +12,44 @@ closeBtn.addEventListener("click", function () {
 });
 // set year
 date.innerHTML = new Date().getFullYear();
+
+/* 
+==========
+Dialog box
+========== 
+*/
+function myConfirmBox(message) {
+  let element = document.createElement("div");
+  element.classList.add("dialog-background");
+  element.innerHTML = `<div class="dialog">
+                            <h3>${message}</h3>
+                            <div>
+                                <button id="trueButton" class="btn btn-primary">Yes</button> <!-- Set Id for both buttons -->
+                                <button id="falseButton" class="btn btn-secondary">No</button>
+                            </div>
+                        </div>`;
+  document.body.appendChild(element);
+  return new Promise(function (resolve, reject) {
+    document
+      .getElementById("trueButton")
+      .addEventListener("click", function () {
+        resolve(true);
+        document.body.removeChild(element);
+      });
+    document
+      .getElementById("falseButton")
+      .addEventListener("click", function () {
+        resolve(false);
+        document.body.removeChild(element);
+      });
+  });
+}
+
+// Using the confirm box
+document.getElementById("logout-btn-id").addEventListener("click", () => {
+  myConfirmBox("Are you sure you want to log out?").then((response) => {
+    if (response) {
+      window.location.replace("http://127.0.0.1:8000/account/logout/");
+    }
+  });
+});
