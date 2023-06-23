1e806653aae09cc343cc79562b035e0a7381d74b
andreistr21 <andstra45@gmail.com>
2023-06-23 20:38:44 +0200

    fixed service shortener test append to redis list

--- src/shortener/tests/services/test_append_to_redis_list.py
+++ src/shortener/tests/services/test_append_to_redis_list.py
@@ -1,4 +1,5 @@
 import datetime
+from functools import lru_cache
 import json
 from unittest import mock
 
@@ -6,12 +7,19 @@ from django.test import TestCase
 from django.utils import timezone
 from fakeredis import FakeStrictRedis
 
-from shortener.redis import redis_connection
+from shortener.services import redis_connection
 from shortener.services import append_to_redis_list
 
 
+@lru_cache(maxsize=1)
+def fake_redis_connection():
+    """Creates redis connection during first call and returns it. During next
+    call cached value will be returned"""
+    return FakeStrictRedis()
+
+
 class AppendToRedisListTests(TestCase):
-    @mock.patch("shortener.redis.Redis", FakeStrictRedis)
+    @mock.patch("shortener.services.redis_connection", fake_redis_connection)
     @mock.patch.object(
         timezone, "now", return_value=datetime.datetime(2023, 6, 23, 18, 20)
     )
@@ -19,7 +27,7 @@ class AppendToRedisListTests(TestCase):
         alias = "test-alias"
         country_code = "PL"
         append_to_redis_list(alias, country_code)
-        redis_con = redis_connection()
+        redis_con = fake_redis_connection()
 
         self.assertEqual(
             redis_con.lrange(alias, 0, 1)[0],
