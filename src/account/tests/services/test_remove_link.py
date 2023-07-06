093e9eb24d759f9ac2f28c4080ee8a2dcb787d8a
andreistr21 <andstra45@gmail.com>
2023-07-06 18:34:37 +0200

    updated tests for remove link account service

--- src/account/tests/services/test_remove_link.py
+++ src/account/tests/services/test_remove_link.py
@@ -1,3 +1,4 @@
+from unittest import mock
 from django.test import TestCase
 
 from account.services import remove_link
@@ -5,10 +6,12 @@ from shortener.models import Link
 
 
 class RemoveLinkTests(TestCase):
-    def test_link_deletion(self):
+    @mock.patch("account.services.remove_link_statistics")
+    def test_link_deletion(self, remove_link_statistics_mock):
         link = Link.objects.create(
             long_link="https://www.youtube.com/", alias="youtube"
         )
         remove_link(link)
 
         self.assertFalse(Link.objects.all().exists())
+        remove_link_statistics_mock.assert_called_once_with(link.alias)
