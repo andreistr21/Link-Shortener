c67ae832dc5c4c7a657438a007f3eb12bc66e3bf
andreistr21 <andstra45@gmail.com>
2023-07-17 19:48:18 +0200

    updated redis connection setup

--- src/account/redis.py
+++ src/account/redis.py
@@ -8,4 +8,4 @@ from redis import Redis
 def redis_connection() -> Redis:
     """Creates redis connection during first call and returns it. During next
     call cached value will be returned"""
-    return Redis(host=getenv("REDIS_HOST"), port="6379")
+    return Redis.from_url(getenv("REDIS_URL"))
