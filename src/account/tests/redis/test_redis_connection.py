d90a4d842053a0fb752a68db57cd7aa260dbc232
andreistr21 <andstra45@gmail.com>
2023-07-27 18:37:33 +0200

    updated redis connection tests for shortener and account apps

--- src/account/tests/redis/test_redis_connection.py
+++ src/account/tests/redis/test_redis_connection.py
@@ -3,14 +3,14 @@ from unittest import mock
 
 from django.test import TestCase
 
-from shortener.redis import redis_connection
+from account.redis import redis_connection
 
 
 class RedisConnectionTests(TestCase):
     def setUp(self) -> None:
         redis_connection.cache_clear()
     
-    @mock.patch("shortener.redis.from_url")
+    @mock.patch("account.redis.from_url")
     def test_redis_non_TSL_connection_caching(self, redis_mock):
         REDIS_URL = "redis_non_TSL_link"
         environ["REDIS_URL"] = REDIS_URL
@@ -20,7 +20,7 @@ class RedisConnectionTests(TestCase):
         redis_mock.assert_called_once_with(REDIS_URL)
         self.assertEqual(connection1, connection2)
 
-    @mock.patch("shortener.redis.from_url")
+    @mock.patch("account.redis.from_url")
     def test_redis_TSL_connection_caching(self, redis_mock):
         REDIS_URL = "rediss://:redis_TSL_link"
         environ["REDIS_URL"] = REDIS_URL
--- src/shortener/redis.py
+++ src/shortener/redis.py
@@ -8,7 +8,6 @@ from redis import Redis, from_url
 def redis_connection() -> Redis:
     """Creates redis connection during first call and returns it. During next
     call cached value will be returned"""
-    # TODO: update tests
     if getenv("REDIS_URL").split("://")[0] == "rediss":
         return from_url(getenv("REDIS_URL"), ssl_cert_reqs=None)
     return from_url(getenv("REDIS_URL"))
